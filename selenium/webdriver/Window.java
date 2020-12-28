26bbfa6d4c424d7579d8ec5f2c7d445e974bd85d
thuyvtp123 <68978194+thuyvtp123@users.noreply.github.com>
2020-12-28 12:29:16 +0700

    Update Window.java

--- selenium/webdriver/Window.java
+++ selenium/webdriver/Window.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -27,20 +29,21 @@ public class Window {
 	By placeHolder = By.xpath("//input[@placeholder='Search apis contain (full text)...']");
 	By btnExplore = By.xpath("//button[@class='btn btn-primary']");
 	By message = By.xpath("//h4[contains(text(),'No API definition provided.')]");
-
+	String currentWindow;
 	String parentID;
 	String url = "http://atlantis.dev.tiki.services/";
 
 	@BeforeTest
 	public void before() {
 
-		// System.setProperty("webdriver.gecko.driver", "chromedriver.exe");
-		// driver = new FirefoxDriver();
-		driver = new ChromeDriver();
+
+		driver = new FirefoxDriver();
+		//driver = new ChromeDriver();
+
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);// cho element dc load
 		driver.manage().window().maximize();// mo full window
 		driver.get(url);
-		// khoi tao data test≈
+
 
 	}
 
@@ -49,28 +52,27 @@ public class Window {
 		driver.quit();
 	}
 
-	// @Test(priority=1)
+	@Test(priority=1)
 	public void VerifyLoginSuccess() throws InterruptedException {
-		parentID = driver.getWindowHandle();
 
 		driver.findElement(btnLogin).click();
 
 		driver.findElement(btnLoginGoogle).click();
 
-		/**
-		 * how-to-switch-to-popup-window-using
-		 */
 
-		System.out.println("current" + parentID);
+		parentID = driver.getWindowHandle();
+		System.out.println("current"+parentID);
 		switchToWindowbyID(parentID);
 
 		WebElement email = driver.findElement(By.id("identifierId"));
-		email.sendKeys("thuytest38@gmail.com");
+
+		email.sendKeys("thuy.vu@tiki.vn");
 		WebElement btnNext1 = driver.findElement(By.xpath("//div[@id='identifierNext']/div/button/div[2]"));
 		btnNext1.click();
+		Thread.sleep(5000);
 
 		WebElement password = driver.findElement(By.name("password"));
-		password.sendKeys("123456");
+		password.sendKeys("Tiennam@12345");
 		WebElement btnNext2 = driver.findElement(By.xpath("//div[@id='passwordNext']/div/button/div[2]"));
 		btnNext2.click();
 
@@ -78,40 +80,50 @@ public class Window {
 
 		// Assert.assertTrue(isAllWindowCloseWithoutParent(parentID));
 		isAllWindowCloseWithoutParent(parentID);
-
 		Assert.assertEquals(
 				driver.findElement(By.xpath("//button[contains(text(),'Thuy (Engineer). Vu Thi Phuong')]")).getText(),
 				"Thuy (Engineer). Vu Thi Phuong");
+		Thread.sleep(5000);
+
 
 	}
 
-	public void VerifyPlaceholder() {
+
+	@Test(priority=2)
+	public void VerifyPlaceholder() throws InterruptedException {
 		String string = driver.findElement(placeHolder).getAttribute("placeholder");
 		System.out.print(string);
 		Assert.assertEquals(string, "Search apis contain (full text)...");
+
+	}
+	@Test(priority=3)
+	public void VerifyClickOnButtonGoForDetail() throws InterruptedException {
+		WebElement btnGoForDetail = driver.findElement(By.xpath("//div[(text()='Atlantis')]/following-sibling::div[@class='card-body']/a[@class='btn btn-primary']"));
+		btnGoForDetail.click();
+		String string=driver.findElement(By.xpath("//h3[contains(text(),'Atlantis')]")).getText();
+		Assert.assertEquals(string, "Atlantis");
+		Thread.sleep(5000);
+
+
 	}
 
-	public void VerifyNavigatePageinterationGuideline() {
+	@Test(priority=4)
+	public void VerifyNavigatePageinterationGuideline() throws InterruptedException {
 		driver.findElement(By.xpath("//button[contains(text(),'Thuy (Engineer). Vu Thi Phuong')]")).click();
 		driver.findElement(interationGuideline).click();
 		Assert.assertEquals(driver.getCurrentUrl(), "http://atlantis.dev.tiki.services/guide-line");
+		Thread.sleep(5000);
 
 	}
-
-	public void VerifyNavigatePagedocViewer() {
+	@Test(priority=5)
+	public void VerifyNavigatePagedocViewer() throws InterruptedException {
 		driver.findElement(By.xpath("//button[contains(text(),'Thuy (Engineer). Vu Thi Phuong')]")).click();
 		driver.findElement(docViewer).click();
 		Assert.assertEquals(driver.getCurrentUrl(), "http://atlantis.dev.tiki.services/api-doc-view");
-
-	}
-
-	public void VerifyWithoutData() throws InterruptedException {
-		driver.findElement(btnExplore).click();
-		driver.findElement(message).getText();
-		Assert.assertEquals(driver.findElement(message).getText(), "No API definition provided.");
 		Thread.sleep(5000);
-	}
 
+	}
+	@Test(priority=6)
 	public void Verify_Atlantis_Link() throws InterruptedException {
 		driver.findElement(By.xpath("//h2[contains(text(),'Atlantis')]")).click();
 		Thread.sleep(5000);
@@ -119,7 +131,28 @@ public class Window {
 
 	}
 
+
+	@Test(priority=7)
+	public void VerifySearch() throws InterruptedException {
+		driver.findElement(placeHolder).clear();
+		driver.findElement(placeHolder).sendKeys("delete");
+		//explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@class='list-group']/button")));
+		Thread.sleep(5000);
+		List<WebElement> allItem = driver.findElements(By.xpath("//div[@class='list-group']/button"));
+		allItem.size();
+		System.out.print("Size" + allItem.size());
+		for (WebElement item : allItem) {
+			String itemText = item.getText().trim();
+			System.out.println(itemText);
+			Assert.assertTrue(itemText.contains("delete"));
+
+		}
+
+	}
+
+	@Test(priority=8)
 	public void VerifyWithoutSearchResult() throws InterruptedException {
+		driver.findElement(placeHolder).clear();
 		driver.findElement(placeHolder).sendKeys("@#$%^&");
 		Thread.sleep(5000);
 		List<WebElement> allItem = driver.findElements(By.xpath("//div[@class='list-group']/button"));
@@ -128,86 +161,43 @@ public class Window {
 		Assert.assertEquals(allItem.size(), 0);
 
 	}
-
-	// @Test(priority=2)
-	public void VerifySearch() throws InterruptedException {
-		driver.findElement(placeHolder).clear();
-		driver.findElement(placeHolder).sendKeys("create");
-		Thread.sleep(5000);
-		List<WebElement> allItem = driver.findElements(By.xpath("//div[@class='list-group']/button"));
-		allItem.size();
-		System.out.print("Size" + allItem.size());
-		for (WebElement item : allItem) {
-			String itemText = item.getText().trim();
-			System.out.println(itemText);
-			Assert.assertTrue(itemText.contains("create"));
-		}
-
-	}
-
-	// @Test(priority=2)
+	@Test(priority=9)
 	public void VerifySelectItemInSearchResult() throws InterruptedException {
-		// driver.findElement(placeHolder).clear();
-		driver.findElement(placeHolder).sendKeys("create");
+		driver.findElement(placeHolder).clear();
+		driver.findElement(placeHolder).sendKeys("delete");
 		Thread.sleep(5000);
-		// explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@class='list-group']/button")));
+		//explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@class='list-group']/button")));
 
 		List<WebElement> allItem = driver.findElements(By.xpath("//div[@class='list-group']/button"));
 
 		for (WebElement item : allItem) {
 			item.click();
-			System.out.print(driver.getCurrentUrl());
-			Assert.assertTrue(driver.getCurrentUrl().contains("http://atlantis.dev.tiki.services/apis?id="));
 
-			Thread.sleep(5000);//chi muốn click hết vào các giá trị show rasau khi search xong ấy em, mà do click vào giá trị nsó chuyển page khác ên chạy sang thang tiep theo bịiail
 			break;
 		}
-	}
-
-	public void VerifySignOut() {
-		driver.findElement(By.xpath("//button[contains(text(),'Thuy (Engineer). Vu Thi Phuong')]")).click();
-		driver.findElement(signOut).click();
-		Assert.assertEquals(driver.findElement(btnLogin).getText(), "Sign In (tikier)");
+		Assert.assertTrue(driver.getCurrentUrl().contains("http://atlantis.dev.tiki.services/apis?id="));
 
 	}
 
-	@Test
-	public void VerifyLoginFail() throws InterruptedException {
-		parentID = driver.getWindowHandle();
+		@Test(priority=10)
+		public void VerifySignOut() throws InterruptedException {
+			driver.findElement(By.xpath("//button[contains(text(),'Thuy (Engineer). Vu Thi Phuong')]")).click();
+			driver.findElement(signOut).click();
+			Thread.sleep(5000);
 
-		driver.findElement(btnLogin).click();
+			Assert.assertEquals(driver.findElement(btnLogin).getText(), "Sign In (tikier)");
+			Thread.sleep(5000);
+		}
 
-		driver.findElement(btnLoginGoogle).click();
 
-		/**
-		 * how-to-switch-to-popup-window-using
-		 */
 
-		System.out.println("current" + parentID);
-		switchToWindowbyID(parentID);
 
-		WebElement email = driver.findElement(By.id("identifierId"));
-		email.sendKeys("doan.thuy.huong@sun-asterisk.com");
-		WebElement btnNext1 = driver.findElement(By.xpath("//div[@id='identifierNext']/div/button/div[2]"));
-		btnNext1.click();
-		Thread.sleep(5000);
-
-		WebElement password = driver.findElement(By.name("password"));
-		password.sendKeys("123456");
-		WebElement btnNext2 = driver.findElement(By.xpath("//div[@id='passwordNext']/div/button/div[2]"));
-		btnNext2.click();
-
-		Thread.sleep(5000);
-		String string = driver.findElement(By.xpath("//span[contains(text(),'Authorization Error')]")).getText();
-
-		Assert.assertEquals(string, "Authorization Error");
-
-	}
-
-	public void switchToWindowbyID(String parentID) {
-		Set<String> allWindows = driver.getWindowHandles();
-		for (String runWindow : allWindows) {
-			if (!runWindow.equals(parentID)) {
+	public void switchToWindowbyID(String parentID)
+	{
+		Set<String> allWindows=driver.getWindowHandles();
+		for(String runWindow: allWindows)
+		{
+			if(!runWindow.equals(parentID)) {
 				driver.switchTo().window(runWindow);
 				break;
 			}
